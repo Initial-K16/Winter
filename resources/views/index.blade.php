982083eebd931e0dabc70064cf55765c59f4b58d
khabib622@smk.belajar.id <khabib622@smk.belajar.id>
2024-10-17 14:31:16 +0700

    masih mencoba

---
+++ resources/views/index.blade.php
@@ -0,0 +1,460 @@
+<!DOCTYPE html>
+<html lang="en"><!-- Basic -->
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+ 
+     <!-- Site Metas -->
+    <title>Christmas - Responsive HTML5 Template</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="#" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="#" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Pogo Slider CSS -->
+    <link rel="stylesheet" href="css/pogo-slider.min.css">
+	<!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css">    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+
+	<!-- LOADER -->
+    <div id="preloader">
+		<div class="loader">
+			<div class="box"></div>
+			<div class="box"></div>
+		</div>
+    </div><!-- end loader -->
+    <!-- END LOADER -->
+	
+	<!-- Start header -->
+	<header class="top-header">
+		<nav class="navbar header-nav navbar-expand-lg">
+            <div class="container">
+				<a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="image"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd" aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-wd">
+                    <ul class="navbar-nav">
+                        <li><a class="nav-link active" href="#home">Beranda</a></li>
+                        <li><a class="nav-link" href="#wish">Harapan</a></li>
+                        <li><a class="nav-link" href="#b-deals">Penawaran Terbaik</a></li>
+						<li><a class="nav-link" href="#donate">Donasi</a></li>
+                        <li><a class="nav-link" href="#testi">Testimoni</a></li>
+						<li><a class="nav-link" href="#contact">Kontak</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+	</header>
+	<!-- End header -->
+	
+	<!-- Start Banner -->
+	<div class="ulockd-home-slider">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="pogoSlider" id="js-main-slider">
+					<div class="pogoSlider-slide" style="background-image:url(images/slider-01.jpg);"></div>
+					<div class="pogoSlider-slide" style="background-image:url(images/slider-01.jpg);"></div>
+				</div><!-- .pogoSlider -->
+			</div>
+		</div>
+	</div>
+	<!-- End Banner -->
+	
+	<!-- wish -->
+	<div id="wish" class="about-box" style="padding-bottom: 0;">
+		<div class="about-a1">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="title-box">
+							<h2>Harapan Terbaik</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12">
+						<div class="row align-items-center about-main-info">
+							
+							<div class="col-lg-6 col-md-6 col-sm-12 text_align_center">
+						      <div class="full">
+						      	  <img class="img-responsive" src="images/w1.png" alt="#" />
+						      </div>
+							</div>
+
+                              <div class="col-lg-6 col-md-6 col-sm-12">
+								<h2><img style="width: 60px;" src="images/head_s.png" alt="#" /> Santa Membuat Harapan</h2>
+								<p>Selamat Natal! Di tengah hangatnya cahaya Natal, mari kita renungkan makna kedamaian dan harapan yang dibawa oleh kelahiran Sang Juru Selamat. Semoga momen istimewa ini mengingatkan kita untuk berbagi kasih dan kebahagiaan dengan orang-orang terkasih. Biarkan setiap tawa dan cerita mengisi hari-hari kita dengan kehangatan dan cinta.</p>
+								<p>Di saat kita merayakan Natal, mari kita ingat untuk saling mendukung dan menguatkan satu sama lain. Semoga semangat Natal menyentuh hati kita, membawa sukacita yang tak terhingga, dan menjadikan dunia ini tempat yang lebih baik. Selamat Natal dan Tahun Baru! Semoga kedamaian dan berkat selalu menyertai langkah kita di tahun yang akan datang.</p>
+								<a href="#" class="hvr-radial-out button-theme">Baca Selengkapnya</a>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+		<div class="about-a1" style="background:#f7f7f7;margin-top: 50px;padding-top: 75px;padding-bottom: 50px;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12">
+						<div class="row align-items-center about-main-info">
+						
+                            <div class="col-lg-6 col-md-6 col-sm-12">
+								<h2><img style="width: 60px;" src="images/head_s.png" alt="#" /> Keinginan jonh Max</h2>
+								<p>Harapan adalah cahaya yang menerangi jalan kita, memberi semangat dalam setiap langkah yang kita ambil. Sejak lama, harapan telah menjadi fondasi bagi setiap impian dan cita-cita, menginspirasi kita untuk terus maju meskipun di tengah tantangan yang menghadang</p>
+								<p>Saat kita menatap masa depan, mari kita ingat bahwa setiap harapan adalah awal dari kemungkinan yang tak terbatas. Dengan keyakinan dan keberanian, kita bisa mengubah setiap tantangan menjadi peluang, meraih apa yang kita impikan, dan menciptakan dunia yang lebih baik bagi diri kita dan orang-orang di sekitar kita.</p>
+								<a href="#" class="hvr-radial-out button-theme">Baca Selengkapnya</a>
+							</div>
+
+							<div class="col-lg-6 col-md-6 col-sm-12 text_align_center">
+						      <div class="full">
+						      	  <img class="img-responsive" src="images/w2.png" alt="#" />
+						      </div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- wish us -->
+	
+	<!-- best deals -->
+	<div id="b-deals" class="services-box main-timeline-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Penawaran Terbaik</h2>
+					</div>
+				</div>
+			</div>
+	
+			<div class="row">
+				<div class="col-lg-6 col-sm-6">
+					<figure class="effect-service">
+						<img src="images/D-1.png" alt="" />
+						<figcaption>
+							<h2>Penawaran 1</h2>
+							<p>Ini adalah contoh teks untuk menunjukkan penawaran terbaik kami.</p>
+							<a href="#"></a>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="col-lg-6 col-sm-6">
+					<figure class="effect-service">
+						<img src="images/D-2.png" alt="" />
+						<figcaption>
+							<h2>Penawaran 2</h2>
+							<p>Ini adalah contoh teks untuk menunjukkan penawaran terbaik kami.</p>
+							<a href="#">Lihat lebih banyak</a>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="col-lg-6 col-sm-6">
+					<figure class="effect-service">
+						<img src="images/D-3.png" alt="" />
+						<figcaption>
+							<h2>Penawaran 3</h2>
+							<p>Ini adalah contoh teks untuk menunjukkan penawaran terbaik kami.</p>
+							<a href="#">Lihat lebih banyak</a>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="col-lg-6 col-sm-6">
+					<figure class="effect-service">
+						<img src="images/D-4.png" alt="" />
+						<figcaption>
+							<h2>Penawaran 4</h2>
+							<p>Ini adalah contoh teks untuk menunjukkan penawaran terbaik kami.</p>
+							<a href="#">Lihat lebih banyak</a>
+						</figcaption>
+					</figure>
+				</div>
+			</div>
+		</div>
+	</div>	
+	<!-- best deals -->
+
+	<!-- donate -->
+	<div id="donate" class="gallery-box" style="background: #f7f7f7;"> 
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Donasi</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<ul class="popup-gallery clearfix">
+					<li>
+						<a href="images/d1.png">
+							<img class="img-fluid" src="images/d1.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/d2.png">
+							<img class="img-fluid" src="images/d2.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/d3.png">
+							<img class="img-fluid" src="images/d3.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/d4.png">
+							<img class="img-fluid" src="images/d4.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/d1.png">
+							<img class="img-fluid" src="images/d1.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/d2.png">
+							<img class="img-fluid" src="images/d2.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/d3.png">
+							<img class="img-fluid" src="images/d3.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/d4.png">
+							<img class="img-fluid" src="images/d4.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/d1.png">
+							<img class="img-fluid" src="images/d1.png" alt="single image">
+							<span class="overlay"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- end donate -->
+	
+	<!-- testimonial -->
+	<div id="testi" class="gallery-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Testimoni</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8 offset-md-2 col-10 offset-1 testimonial_slider">
+					<div id="client_slider" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#client_slider" data-slide-to="0" class="active"></li>
+							<li data-target="#client_slider" data-slide-to="1"></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item text-center active">
+								<div class="testimonial_img">
+									<img width="150" height="150" class="d-block rounded-circle" src="http://nicesnippets.com/demo/profile-1.jpg" alt="First slide">
+								</div>
+								<h5 class="mt-4 mb-0"><strong class="text-uppercase" style="color: #e91327;padding-bottom: 0;">Harry Mike</strong></h5>
+								<p class="m-0 pt-3">Pengalaman saya sangat luar biasa! Pelayanan yang diberikan sangat memuaskan dan hasilnya melebihi ekspektasi saya.</p>
+							</div>
+							<div class="carousel-item text-center">
+								<div class="testimonial_img">
+									<img width="150" height="150" class="d-block rounded-circle" src="http://nicesnippets.com/demo/profile-3.jpg" alt="Second slide">
+								</div>
+								<h5 class="mt-4 mb-0"><strong class="text-uppercase" style="color: #e91327;padding-bottom: 0;">Jonh Max</strong></h5>
+								<p class="m-0 pt-3">Saya sangat merekomendasikan layanan ini. Timnya profesional dan sangat membantu dalam memenuhi kebutuhan saya.</p>
+							</div>
+						</div>
+					</div>
+				</div>  
+			</div>
+		</div>
+	</div>	
+	<!-- end testimonial -->
+	
+	
+	<!-- contact -->
+	<div id="contact" class="contact-box" style="background: #f7f7f7;">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Kontak Kami</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				
+				<div class="col-lg-7 col-sm-7 col-xs-12">
+				  <div class="contact-block">
+					<form id="contactForm">
+					  <div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<input type="text" class="form-control" id="name" name="name" placeholder="Nama" required data-error="Please enter your name">
+								<div class="help-block with-errors"></div>
+							</div>                                 
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<input type="text" placeholder="Email" id="email" class="form-control" name="name" required data-error="Please enter your email">
+								<div class="help-block with-errors"></div>
+							</div> 
+						</div>
+						<div class="col-md-12">
+							<div class="form-group">
+								<input type="text" placeholder="Nomer" id="number" class="form-control" name="number" required data-error="Please enter your number">
+								<div class="help-block with-errors"></div>
+							</div> 
+						</div>
+						<div class="col-md-12">
+							<div class="form-group"> 
+								<textarea class="form-control" id="message" placeholder="Pesan" rows="8" data-error="Write your message" required></textarea>
+								<div class="help-block with-errors"></div>
+							</div>
+							<div class="submit-button text-center">
+								<button class="btn btn-common" id="submit" type="submit">Kirim Pesan</button>
+								<div id="msgSubmit" class="h3 text-center hidden"></div> 
+								<div class="clearfix"></div> 
+							</div>
+						</div>
+					  </div>            
+					</form>
+				  </div>
+				</div>
+
+
+				<div class="col-lg-5 col-sm-5 col-xs-12">
+					<div class="left-contact">
+						<div class="media cont-line">
+							<div class="media-left icon-b">
+								<i class="fa fa-location-arrow" aria-hidden="true"></i>
+							</div>
+							<div class="media-body dit-right">
+								<h4>Alamat</h4>
+								<p>Fleming 196 Woodside Circle Mobile, FL 36602</p>
+							</div>
+						</div>
+						<div class="media cont-line">
+							<div class="media-left icon-b">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+							<div class="media-body dit-right">
+								<h4>Email</h4>
+								<a href="#">demoinfo@gmail.com</a><br>
+								<a href="#">demoinfo@gmail.com</a>
+							</div>
+						</div>
+						<div class="media cont-line">
+							<div class="media-left icon-b">
+								<i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+							</div>
+							<div class="media-body dit-right">
+								<h4>Nomor HP</h4>
+								<a href="#">12345 67890</a><br>
+								<a href="#">12345 67890</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+	<!-- end contact -->
+	
+	<!-- Start Subscribe -->
+	<div class="subscribe-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="subscribe-inner text-center clearfix">
+						<h2>Subscribe</h2>
+						<form action="#" method="post">
+							<div class="form-group">
+								<input class="form-control-1" id="email-1" name="email" placeholder="Alamat Email" required="" type="text">
+							</div>
+							<div class="form-group">
+								<button type="submit" class="hvr-radial-out">
+									Subscribe
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Subscribe -->
+	
+	<!-- Start Footer -->
+	<footer class="footer-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="footer-company-name">Hak Cipta Dilindungi. &copy; 2018 <a href="#">Evento Christmas</a> Desain Oleh: <a href="https://html.design/">html design</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>	
+	<!-- End Footer -->
+	
+	<a href="#" id="scroll-to-top" class="hvr-radial-out"><i class="fa fa-angle-up"></i></a>
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.pogo-slider.min.js"></script> 
+	<script src="js/slider-index.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+	<script src="js/isotope.min.js"></script>	
+	<script src="js/images-loded.min.js"></script>	
+    <script src="js/custom.js"></script>
+</body>
+</html>
